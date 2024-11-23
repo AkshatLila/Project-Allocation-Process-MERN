370c9ca8893bf8b02b74c2ba196faa58483776f1
Bhavya Jain <jainbhavya2306@gmail.com>
2024-11-23 20:39:43 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link,
-  Form
-}
-from "react-router-dom"
-import TeamStatus from './component/TeamStatusAdmin';
-import Login from './component/Login';
+  Form,
+} from "react-router-dom";
+import TeamStatus from "./component/TeamStatusAdmin";
+import Login from "./component/Login";
 import StudentPortal from "./component/Studentportal";
-import StudentPortalStatus from './component/StudentprojectStatus';
-import AdminPortal from './component/AdminPortal';
-import ProjectAbstractForm from './component/Form1';
+import StudentPortalStatus from "./component/StudentprojectStatus";
+import AdminPortal from "./component/AdminPortal";
+import ProjectAbstractForm from "./component/Form1";
 import RoleSpecificationForm from "./component/Form2";
 import WeeklyStatusMatrix from "./component/Form3";
 import MentorPortal from "./component/MentorPortal";
@@ -25,13 +23,13 @@ import AdminUploadSection from "./component/AdminUpload";
 function App() {
   return (
     <>
-    <Router>
-    <Routes>
-    <Route path = '/' element = {<AdminUploadSection/>}/>
-    <Route path = '/studentstatus' element = {<StudentPortalStatus/>}/>
-    <Route path = '/teamstatus' element = {<TeamStatus/>}/>
-    </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/studentstatus" element={<StudentPortalStatus />} />
+          <Route path="/teamstatus" element={<TeamStatus />} />
+        </Routes>
+      </Router>
     </>
   );
 }
